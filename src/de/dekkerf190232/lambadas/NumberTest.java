68487081e781e64cc6e617835fad15e9ed636bb1
Sirvierl0ffel <iminternetimmeranonym@gmail.com>
2022-09-26 09:10:24 +0200

    NumberTester definition

---
+++ src/de/dekkerf190232/lambadas/NumberTest.java
@@ -0,0 +1,7 @@
+package de.dekkerf190232.lambadas;
+
+public interface NumberTest {
+
+    boolean testNumber(int number);
+
+}
---
+++ src/de/dekkerf190232/lambadas/NumberTester.java
@@ -0,0 +1,19 @@
+package de.dekkerf190232.lambadas;
+
+public class NumberTester {
+
+    private final String fileName;
+
+    private NumberTest oddTester;
+    private NumberTest primeTester;
+    private NumberTest palindromeTester;
+
+    public NumberTester(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public void testFile() {
+
+    }
+
+}
