e0fd086b67292a4233cfad691f1b72c472260d15
Sirvierl0ffel <iminternetimmeranonym@gmail.com>
2022-09-26 09:32:22 +0200

    NumberTester tests with lambadas complete

--- src/de/dekkerf190232/lambadas/Main.java
+++ src/de/dekkerf190232/lambadas/Main.java
@@ -4,6 +4,26 @@ public class Main {
 
     public static void main(String[] args) {
 //        HalloJavaMitForEach.example1();
+        NumberTester tester = new NumberTester("tests.txt");
+        tester.setOddEvenTester(n -> n % 2 == 0);
+        tester.setPrimeTester(n -> {
+            if (n <= 1) return false;
+            for (int i = 2; i * i <= n; i++) {
+                if (n % i == 0) return false;
+            }
+            return true;
+        });
+        tester.setPalindromeTester(n -> {
+            int palindrome = n;
+            int reverse = 0;
+            while (palindrome != 0) {
+                int remainder = palindrome % 10;
+                reverse = reverse * 10 + remainder;
+                palindrome = palindrome / 10;
+            }
+            return n == reverse;
+        });
+        tester.testFile();
     }
 
 }
--- src/de/dekkerf190232/lambadas/NumberTester.java
+++ src/de/dekkerf190232/lambadas/NumberTester.java
@@ -1,6 +1,14 @@
 package de.dekkerf190232.lambadas;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class NumberTester {
+
+    public static final int TEST_EVEN = 1;
+    public static final int TEST_PRIME = 2;
+    public static final int TEST_PALINDROME = 3;
 
     private final String fileName;
 
@@ -12,8 +20,47 @@ public class NumberTester {
         this.fileName = fileName;
     }
 
-    public void testFile() {
+    public void setOddEvenTester(NumberTest oddTester) {
+        this.oddTester = oddTester;
+    }
 
+    public void setPrimeTester(NumberTest primeTester) {
+        this.primeTester = primeTester;
+    }
+
+    public void setPalindromeTester(NumberTest palindromeTester) {
+        this.palindromeTester = palindromeTester;
+    }
+
+    public void testFile() {
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            int count = 0;
+            while ((line = br.readLine()) != null) {
+                String[] split = line.split(" ");
+                if (split.length == 1) {
+                    count = Integer.parseInt(line);
+                    continue;
+                }
+                int n1 = Integer.parseInt(split[0]);
+                int n2 = Integer.parseInt(split[1]);
+                switch (n1) {
+                    case TEST_EVEN:
+                        System.out.println("even(" + n2 + ") = " + oddTester.testNumber(n2));
+                        break;
+                    case TEST_PRIME:
+                        System.out.println("prime(" + n2 + ") = " + primeTester.testNumber(n2));
+                        break;
+                    case TEST_PALINDROME:
+                        System.out.println("palindrome(" + n2 + ") = " + palindromeTester.testNumber(n2));
+                        break;
+                    default:
+                        throw new RuntimeException();
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
